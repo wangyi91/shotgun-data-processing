b18ee8e386211b10936af2047bce118db9b9145d
Yi Wang <wangyi9122@yahoo.com>
2024-06-29 10:48:08 +0200

    reset repo

--- BinAC_scripts/4_rsync.sh
+++
@@ -1,5 +0,0 @@
-
-# run in interactive shell
-# manage 10 files at a time
-rsync -avP  /beegfs/work/kn_pop524719/HIBO_shotgun/fastp_sga_output/AVXF-1-0[1-9].sga4.filter.pass.fa.gz tvg137@ssh-snm-willerslev.science.ku.dk:/willerslev/users-shared/science-snm-willerslev-tvg137/HIBO_shotgun/sga/
-
